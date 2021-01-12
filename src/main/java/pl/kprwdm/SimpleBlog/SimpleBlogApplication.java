cc48a0297708d2f553b752d595ab4214f0a18420
Tomek Piasek <tomek.piasek@gmail.com>
2021-01-12 14:42:50 +0100

    Added "extends SpringBootServletInitializer" to SimpleBlogApplication class in order to fix Tomcat 404 error

--- src/main/java/pl/kprwdm/SimpleBlog/SimpleBlogApplication.java
+++ src/main/java/pl/kprwdm/SimpleBlog/SimpleBlogApplication.java
@@ -2,9 +2,10 @@ package pl.kprwdm.SimpleBlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleBlogApplication {
+public class SimpleBlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleBlogApplication.class, args);
