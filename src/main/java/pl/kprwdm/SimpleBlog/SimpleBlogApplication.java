99d90ef4cc9252c01be5d727778a67e4dc0d5a22
Tomek Piasek <tomek.piasek@gmail.com>
2021-01-12 12:39:43 +0100

    Optimized SimpleBlogApplication.java imports

--- src/main/java/pl/kprwdm/SimpleBlog/SimpleBlogApplication.java
+++ src/main/java/pl/kprwdm/SimpleBlog/SimpleBlogApplication.java
@@ -1,11 +1,7 @@
 package pl.kprwdm.SimpleBlog;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SimpleBlogApplication {
